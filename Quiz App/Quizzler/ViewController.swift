d50bdd37f88b15b45460afb112346fe637aca42d
Anmol Jain <itsmeaj20@gmail.com>
2019-07-21 22:03:00 +0530

    added questions, working score system

--- Quiz App/Quizzler/ViewController.swift
+++ Quiz App/Quizzler/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestion: QuestionBank = QuestionBank()
+    var questionNumber: Int = 0
+    var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,27 +22,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        questionLabel.text = allQuestion.list[questionNumber].questionText
+        progressLabel.text = String(questionNumber+1) + "/13"
+        scoreLabel.text = String(score)
     }
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            checkAnswer(answer: true)
+        } else if sender.tag == 2 {
+            checkAnswer(answer: false)
+        }
     }
     
     func updateUI() {
-      
+
     }
 
     func nextQuestion() {
-        
+        questionNumber += 1
+        if questionNumber >= 13 {
+            startOver()
+        }
+        questionLabel.text = allQuestion.list[questionNumber].questionText
+        progressLabel.text = String(questionNumber + 1) + "/13"
+        scoreLabel.text = String(score)
     }
     
-    func checkAnswer() {
-        
+    func checkAnswer(answer:Bool) {
+        if allQuestion.list[questionNumber].answer == answer {
+            score += 1
+        }
+        nextQuestion()
     }
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
     }
     
 }
