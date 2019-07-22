f81fdd7c241c459ae9acdc21c04cb86dabaf68dd
Anmol Jain <itsmeaj20@gmail.com>
2019-07-22 17:53:33 +0530

    Made quiz app perfect & complete, progress bar is now working

--- Quiz App/Quizzler/Question.swift
+++ Quiz App/Quizzler/Question.swift
@@ -18,6 +18,4 @@ class Question {
         answer = correctAnswer
     }
     
-    
-    
 }
--- Quiz App/Quizzler/ViewController.swift
+++ Quiz App/Quizzler/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     let allQuestion: QuestionBank = QuestionBank()
-    var questionNumber: Int = 0
+    var questionNumber: Int = -1
     var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -22,9 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = allQuestion.list[questionNumber].questionText
-        progressLabel.text = String(questionNumber+1) + "/13"
-        scoreLabel.text = String(score)
+        nextQuestion()
     }
 
     @IBAction func answerPressed(_ sender: AnyObject) {
@@ -33,32 +31,50 @@ class ViewController: UIViewController {
         } else if sender.tag == 2 {
             checkAnswer(answer: false)
         }
+        
+        nextQuestion()
     }
     
     func updateUI() {
-
+        questionLabel.text = allQuestion.list[questionNumber].questionText
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
 
     func nextQuestion() {
         questionNumber += 1
-        if questionNumber >= 13 {
-            startOver()
+        
+        if questionNumber < 13 {
+            updateUI()
         }
-        questionLabel.text = allQuestion.list[questionNumber].questionText
-        progressLabel.text = String(questionNumber + 1) + "/13"
-        scoreLabel.text = String(score)
+        
+        else {
+            let alert = UIAlertController(title: "Awesome", message: "You finished all the questions, your final score was \(score), do you want to start over", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     func checkAnswer(answer:Bool) {
         if allQuestion.list[questionNumber].answer == answer {
             score += 1
+            updateUI()
+            print("You got it")
+        } else {
+            print("wrong answer")
         }
-        nextQuestion()
+        
     }
     
     func startOver() {
-        questionNumber = 0
+        questionNumber = -1
         score = 0
+        nextQuestion()
     }
     
 }
