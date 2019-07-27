8ffca598830ae981ce7bf3c1d031ca3b2b9d2b46
Anmol Jain <itsmeaj20@gmail.com>
2019-07-27 23:21:16 +0530

    Added API Key to make an HTTP Get Request using Alamofire to get the weather data given the latitude and longitude of the iPhone Device given by the locationManager

--- Clima/Clima/WeatherViewController.swift
+++ Clima/Clima/WeatherViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
-    /***Get your own App ID at https://openweathermap.org/appid ****/
+    let APP_ID = "a96e270ebbf9b0048c1c41d3e9a838d7"
+    /***Get your own App ID at https:openweathermap.org/appid ****/
     
 
     //TODO: Declare instance variables here
-    
+    var locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -32,7 +34,10 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
         
     }
@@ -43,7 +48,21 @@ class WeatherViewController: UIViewController {
     /***************************************************************/
     
     //Write the getWeatherData method here:
-    
+    func getWeatherData(URL:String, parameter: [String: String]) {
+        
+        Alamofire.request(URL, method: .get, parameters: parameter).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success")
+            }
+            
+            else {
+                print("Server didn't responded correctly")
+                self.cityLabel.text = "Connection Issue"
+            }
+        }
+        
+    }
 
     
     
@@ -77,10 +96,29 @@ class WeatherViewController: UIViewController {
     
     //Write the didUpdateLocations method here:
     
-    
+    //This function is triggered when the locationManager is able to successfully retrieve the location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            //Important because location manager is a very battery intensive process and should be stopped when done
+            locationManager.stopUpdatingLocation()
+            print("Location: (\(location.coordinate.longitude), \(location.coordinate.latitude))")
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            
+            let params: [String : String] = ["lat": String(latitude), "lon": String(longitude), "appid": APP_ID ]
+            
+            getWeatherData(URL: WEATHER_URL, parameters: params)
+        }
+    }
     
     //Write the didFailWithError method here:
-    
+    //This function is triggered when the locationManager is unable to retrieve the location
+    //due to any reasons like, No Internet, is in airplane mode, permission not granted
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
