b2e9231a907b012eae830648e90b9159722b6871
Anmol Jain <itsmeaj20@gmail.com>
2019-07-28 20:47:39 +0530

    added working code for sending data from one ViewController to another

--- segues/segues/SecondViewController.swift
+++ segues/segues/SecondViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var textboxData : String?
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let data = textboxData {
+            label.text = data
+        }
         // Do any additional setup after loading the view.
     }
     
