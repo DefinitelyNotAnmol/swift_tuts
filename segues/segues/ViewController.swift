7a6237f2de9d5c04cefadf686390cde5092062d4
Anmol Jain <itsmeaj20@gmail.com>
2019-07-28 20:45:26 +0530

    working segues, mulitple ViewControllers, how to send from one ViewController to another, how to set multiple segues from one ViewController

---
+++ segues/segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segues
+//
+//  Created by Anmol Jain on 28/07/19.
+//  Copyright © 2019 Anmol Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
