508180be2cbb64d2052a5b62561edaf260546adf
Anmol Jain <itsmeaj20@gmail.com>
2019-07-29 19:31:31 +0530

    added Flashy Tabbar UI developed by Cuberto

---
+++ Cuberto-UI/FlashyTabBar/Example/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  CBFlashyTabBarController
+//
+//  Created by askopin@gmail.com on 11/29/2018.
+//  Copyright (c) 2018 askopin@gmail.com. All rights reserved.
+//
+
+import UIKit
+import CBFlashyTabBarController
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        CBFlashyTabBar.appearance().tintColor = #colorLiteral(red: 0.1176470588, green: 0.1176470588, blue: 0.431372549, alpha: 1)
+        CBFlashyTabBar.appearance().barTintColor = .white
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+}
