0b2aed9103e9d2a3f68e56dd435768c9cf29df81
Anmol Jain <itsmeaj20@gmail.com>
2019-09-09 17:25:30 +0530

    App without interface builder [more professional way to make an app]

---
+++ NoStoryBoard/NoStoryBoard/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  NoStoryBoard
+//
+//  Created by Anmol Jain on 09/09/19.
+//  Copyright © 2019 Anmol Jain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.orange
+    }
+    
+}
---
+++ NoStoryBoard/NoStoryBoard/SecondaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondaryViewController.swift
+//  NoStoryBoard
+//
+//  Created by Anmol Jain on 09/09/19.
+//  Copyright © 2019 Anmol Jain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondaryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+    
+}
